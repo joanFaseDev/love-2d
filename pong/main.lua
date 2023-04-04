9a8ba378c82dcbb073922ba0c70bbe8eed9e1e54
joan_fase <jordanfalaise.dev@gmail.com>
2023-04-04 15:13:46 +0200

    Added paddles & quit game through escape key

--- pong/main.lua
+++ pong/main.lua
@@ -9,26 +9,33 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
-TITLE = "Pong"
+TITLE = "Welcome Pong!"
 FONT_AUDIOWIDE = 'assets/fonts/Audiowide-Regular.ttf'
 FONT_PRESSSTART = 'assets/fonts/PressStart2P-Regular.ttf'
 
--- setNewFont() combines creating/setting a new font only works with TTF fonts
--- FONT = love.graphics.setNewFont(FONT_PRESSSTART, 30)
-FONT = love.graphics.newFont(FONT_PRESSSTART, 30)
-TITLE_WIDTH = FONT:getWidth(TITLE)
-TITLE_HEIGHT = FONT:getHeight(TITLE)
+titleWidth = nil
+titleHeight = nil
 
+recWidth = 5
+recHeight = 20
 --[[
     Runs when the game first starts up, only once; used to initialize the game
 ]]
 function love.load()
-    --[[ 
-        This function does not apply retroactively to loaded images.
-        Already existing objects retain their current scaling filters
-    ]]
+--[[ 
+    This function does not apply retroactively to loaded images.
+    Already existing objects retain their current scaling filters
+]]
     love.graphics.setDefaultFilter('nearest', 'nearest')
+                
+    -- setNewFont() combines creating/setting a new font only works with TTF fonts
+    -- FONT = love.graphics.setNewFont(FONT_PRESSSTART, 30)
+    FONT = love.graphics.newFont(FONT_PRESSSTART, 8)
+    love.graphics.setFont(FONT)
 
+    titleWidth = FONT:getWidth(TITLE)
+    titleHeight = FONT:getHeight(TITLE)
+    
     -- Initialize the virtual resolution which will be rendered within the actual window
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -36,12 +43,25 @@ function love.load()
         vsync = true
     })
     
-    love.graphics.setFont(FONT)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.draw()
     -- Apply the virtual resolution
     push:apply('start')
-    love.graphics.printf(TITLE, 0, VIRTUAL_HEIGHT / 2 - TITLE_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+
+    -- Clear the screen to a specified color
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+    -- To change font color, add a table as a first argument like this {{r, g, b, a}, string1, {r, g, b, a}, string2, ...}
+    love.graphics.printf(TITLE, 0 , titleHeight, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.rectangle('fill', 10, 30, recWidth, recHeight)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - (10 +recWidth), VIRTUAL_HEIGHT - (30 + recHeight), recWidth, recHeight)
+    
     push:apply('end')
 end
